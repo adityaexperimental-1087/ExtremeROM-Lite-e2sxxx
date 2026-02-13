4ddacf7efda40c0f08174d4510776fb16c3cc738
Aditya <adityaexperimental@gmail.com>
2026-02-13 12:50:26 +0530

    Change FW to Oneui 5.1

--- scripts/download_fw.sh
+++ scripts/download_fw.sh
@@ -174,7 +174,7 @@ for i in "${FIRMWARES[@]}"; do
         (
         cd "$OUT_DIR"
         STR=""
-        [ $MODEL == "SM-S731B" ] && STR=" -v S731BXXU1AYH9/S731BOXM1AYH9/S731BXXU1AYH9/S731BXXU1AYH9"
+        [ $MODEL == "SM-S906B" ] && STR=" -v S906BXXS4CWD3/S906BOXM4CWCH/S906BXXS4CWD3/S906BXXS4CWD3"
         samloader -m "$MODEL" -r "$CSC" -i "$IMEI" -s "$SERIAL_NO" download$STR -O "$ODIN_DIR/${MODEL}_${CSC}" || exit 1
         )
 
